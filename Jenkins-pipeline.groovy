75fe0dd9a2b497d166926552237e192bdc4bc7b9
saroj-naik18 <168340647+saroj-naik18@users.noreply.github.com>
2024-05-30 11:40:36 +0530

    jenkins-pipeline

---
+++ Jenkins-pipeline.groovy
@@ -0,0 +1,26 @@
+
+pipeline {
+    agent any{
+        stages{
+            stage("build")
+            steps {
+                echo 'builiding the application'
+
+            }
+        }
+                stages{
+            stage("test")
+            steps {
+                echo 'testing the app'
+
+            }
+        }
+                stages{
+            stage("deploy")
+            steps {
+                echo'deploying the app'
+
+            }
+        }
+    }
+}
